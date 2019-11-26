9f00ea1f6d01809e2337810b6dbe0087a54f5a0d
Shmulik <syraices@gmail.com>
2019-11-26 03:06:53 +0000

    Components

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class List extends React.Component{
+  render(){
+    return(
+      <div className="list-container">
+        List component
+      </div>
+    );
+  }
+}
+
+export default List;
